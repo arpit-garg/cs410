993135893f0ae3fb7f902d1149a98d3d5f88a464
Garima Lalwani <Garima@Garimas-MacBook-Pro.local>
2017-05-01 18:55:45 -0500

    py and js scripts

---
+++ src/toolkit/variables.py
@@ -0,0 +1,20 @@
+'''
+Created on Feb 13, 2013
+
+@author: xiaolong
+'''
+
+# HOME_DIR = '/home/xwang95/';
+HOME_DIR = '/Users/Garima';
+# /Users/xiaolong/
+
+# SRC_DIR = '/home/xwang95/workspace/src/';
+SRC_DIR = '/Users/Garima/Documents/workspace/citation_lda/src';
+# DATA_DIR = '/home/xwang95/data/';
+DATA_DIR = '/Users/Garima/Documents/workspace/citation_lda/data';
+RESOURCE_DIR = '/home/xwang95/resource/';
+RESULT_DIR = '/home/xwang95/result/';
+RUN_DIR = '/home/xwang95/run/';
+
+if __name__ == '__main__':
+    pass
